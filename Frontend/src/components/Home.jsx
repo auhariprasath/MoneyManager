100a5d7f35e8d39e5a59a13efdbc31c0fbe3d39a
Hariprasath A U <hari24prasath2005@gmail.com>
2025-12-23 00:21:02 +0530

    frontend-setup

---
+++ Frontend/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <>
+        <button>Profile</button>
+        <center> <h1>Welcome to Money Manager</h1> </center>
+        <p>current_balance: </p>
+        <button>Transactions</button>
+        <button>Analytics</button>
+        <button>Budget</button>
+    </>
+  )
+}
