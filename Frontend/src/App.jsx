f3ae1e1a81956601575c173977ae8d6a5c6afc57
Hariprasath A U <hari24prasath2005@gmail.com>
2025-12-24 22:46:25 +0530

    route set up

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,24 @@
-import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Login from './components/Login.jsx'
 import { Home } from './components/Home.jsx'
 import Transaction from './components/Transaction.jsx'
 import Budget from './components/Budget.jsx'
 import Analytics from './components/Analytics.jsx'
+import Profile from './components/Profile.jsx'
 
 function App() {
-
   return (
-    <>
+    <div>
+      
       <Routes>
-        <p>huygrtc</p>
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/" element={<Home />} />
         <Route path="/transactions" element={<Transaction />} />
         <Route path="/budget" element={<Budget />} />
         <Route path="/analytics" element={<Analytics />} />
       </Routes>
-    </>
+    </div>
   )
 }
 
--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-
+import { useNavigate } from "react-router-dom"
 export const Home = () => {
+  const navigate = useNavigate(); 
   return (
     <>
-        <button>Profile</button>
-        <center> <h1>Welcome to Money Manager</h1> </center>
-        <p>current_balance: </p>
-        <button>Transactions</button>
-        <button>Analytics</button>
-        <button>Budget</button>
+      <button onClick ={()=> navigate('/profile')}>Profile</button>
+      <h1 style={{ textAlign: 'center' }}>Welcome to Money Manager</h1>
+      <p>current_balance:</p>
+      <button onClick={() => navigate("/transactions")}>Transactions</button>
+      <button onClick={() => navigate("/analytics")}>Analytics</button>
+      <button onClick={() => navigate("/budget")}>Budget</button>
     </>
   )
 }
